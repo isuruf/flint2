594d0f429705d6616e6c5da55f803e58783b8b05
Isuru Fernando <isuruf@gmail.com>
2016-07-03 23:03:03 +0530

    test

--- .appveyor_msvc_build.cmd
+++ .appveyor_msvc_build.cmd
@@ -1,9 +1,9 @@
 cd build.vc14\flint_config
-python %cd%\flint_config.py --build-lib True --build-tests False --build-profiles False
+python %cd%\flint_config.py --build-dll True --build-tests False --build-profiles False
 
 cd ..
-msbuild.exe lib_flint/lib_flint.vcxproj /p:Configuration=Release /p:Platform=%PLATFORM% /p:PlatformToolset=v140 /verbosity:minimal
-copy lib_flint\%PLATFORM%\Release\lib_flint.lib ..\lib\%PLATFORM%\Release\lib_flint.lib
+msbuild.exe dll_flint/dll_flint.vcxproj /p:Configuration=Release /p:Platform=%PLATFORM% /p:PlatformToolset=v140 /verbosity:minimal
+copy dll_flint\%PLATFORM%\Release\dll_flint.lib ..\dll\%PLATFORM%\Release\dll_flint.lib
 
 cd build_tests
 python %cd%\build_tests.py --interfaces-tests False --platform %PLATFORM%
--- .build_dependencies.cmd
+++ .build_dependencies.cmd
@@ -6,14 +6,14 @@ curl -fsS -o mpir.zip http://mpir.org/mpir-2.7.2.zip
 7z x mpir.zip > NUL
 rename mpir-2.7.2 mpir
 cd mpir\build.vc14
-msbuild.exe lib_mpir_gc/lib_mpir_gc.vcxproj /p:Configuration=Release /p:Platform=%PLATFORM% /p:PlatformToolset=v140 /verbosity:minimal
+msbuild.exe dll_mpir_gc/dll_mpir_gc.vcxproj /p:Configuration=Release /p:Platform=%PLATFORM% /p:PlatformToolset=v140 /verbosity:minimal
 cd ..\..
 
 curl -fsSL -o mpfr.zip https://github.com/BrianGladman/mpfr/archive/master.zip
 7z x mpfr.zip > NUL
 rename mpfr-master mpfr
 cd mpfr\build.vc14
-msbuild.exe lib_mpfr.sln /p:Configuration=Release /p:Platform=%PLATFORM% /p:PlatformToolset=v140 /verbosity:minimal
+msbuild.exe dll_mpfr.sln /p:Configuration=Release /p:Platform=%PLATFORM% /p:PlatformToolset=v140 /verbosity:minimal
 cd ..\..
 
 curl -fsSL -o pthreads.zip https://github.com/BrianGladman/pthreads/archive/master.zip
